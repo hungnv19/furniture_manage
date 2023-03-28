5dd6cc99e48e76c3b32c19f5ad43a8c39d980e81
hungnp19072002 <hungnv19072002@gmail.com>
2023-03-28 15:58:04 +0700

    Add User Seeder

---
+++ database/seeders/UsersSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = new User();
+        $user->name = 'admin';
+        $user->email = 'admin@gmail.com';
+        $user->password = Hash::make('12345678');
+        $user->save();
+    }
+}
