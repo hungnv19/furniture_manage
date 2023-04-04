ae533eb5d186b3c137793c7f82addc8d7efb7907
Hungnp02 <hungnv19072002@gmail.com>
2023-04-04 16:14:05 +0700

    update

--- resources/views/layout/sliderbar.blade.php
+++ resources/views/layout/sliderbar.blade.php
@@ -174,7 +174,6 @@
 
                     </ul>
                 </li>
-               
                 <li class="nav-item">
                     <a href="#" class="nav-link">
                         <i class="nav-icon fas fa-tachometer-alt"></i>
